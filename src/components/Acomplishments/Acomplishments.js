2e9b08a781606eb675bc0d6c10256b3d16402b72
Rayen Bedoui <altarius1998@gmail.com>
2022-03-22 03:28:32 +0100

    starting work on the accomplishments section

--- src/components/Acomplishments/Acomplishments.js
+++ src/components/Acomplishments/Acomplishments.js
@@ -1,26 +1,14 @@
 import React from 'react';
 
-import { Section, SectionDivider, SectionTitle } from '../../styles/GlobalComponents';
-import { Box, Boxes, BoxNum, BoxText } from './AcomplishmentsStyles';
+import { Section, SectionDivider, SectionSubText, SectionTitle } from '../../styles/GlobalComponents';
 
-const data = [
-  { number: 20, text: 'Open Source Projects'},
-  { number: 1000, text: 'Students', },
-  { number: 1900, text: 'Github Followers', },
-  { number: 5000, text: 'Github Stars', }
-];
+
 
 const Acomplishments = () => (
   <Section>
     <SectionTitle>Personal Achievements</SectionTitle>
-    <Boxes>
-      {data.map((card, index) => (
-        <Box key={index}>
-          <BoxNum>{`${card.number}+`}</BoxNum>
-          <BoxText>{card.text}</BoxText>
-        </Box>
-      ))}
-    </Boxes>
+    <label Bold>TryhackMe</label>
+    <label >HackTheBox</label>
     <SectionDivider/>
   </Section>
 );
